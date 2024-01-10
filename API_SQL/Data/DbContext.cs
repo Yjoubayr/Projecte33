1613e3b19c451bc44deddeaaa360e730671f21fc
janEstrada24 <jestrada48@boscdelacoma.cat>
2024-01-10 17:56:45 +0100

    Configuracio de les cancons afegida

--- API_SQL/Data/DbContext.cs
+++ API_SQL/Data/DbContext.cs
@@ -5,7 +5,6 @@ namespace mba.basquet {
     public class DataContext : DbContext {
         public DataContext(DbContextOptions<DataContext> options) : base(options) { }
         public DbSet<Canco> Cancons { get; set; }
-        
         public DbSet<Album> Albums { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
