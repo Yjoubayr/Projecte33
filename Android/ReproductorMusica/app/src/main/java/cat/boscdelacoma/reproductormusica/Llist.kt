06ac98068bd263e9e2141d06293208a6acedb644
Youssef-ai-svg <yfdil690@boscdelacoma.cat>
2024-01-10 18:13:44 +0100

    Pantalla Main on es reprudueixen les cançons feta

---
+++ Android/ReproductorMusica/app/src/main/java/cat/boscdelacoma/reproductormusica/Llist.kt
@@ -0,0 +1,11 @@
+package cat.boscdelacoma.reproductormusica
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Llist : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_llist)
+    }
+}
