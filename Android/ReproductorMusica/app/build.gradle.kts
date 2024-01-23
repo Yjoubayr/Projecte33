a02ba955b261c5ecac5a78c132fed20945d813e2
Youssef-ai-svg <yfdil690@boscdelacoma.cat>
2024-01-23 15:16:06 +0100

    Creacio dels metodes de get i post d'una canço a mongo (falta fer probes)

--- Android/ReproductorMusica/app/build.gradle.kts
+++ Android/ReproductorMusica/app/build.gradle.kts
@@ -48,6 +48,7 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation ("com.squareup.retrofit2:retrofit:2.9.0")
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.github.kittinunf.fuel:fuel:2.3.1")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
--- Android/ReproductorMusica/app/src/main/java/cat/boscdelacoma/reproductormusica/HTTP_Mongo.kt
+++ Android/ReproductorMusica/app/src/main/java/cat/boscdelacoma/reproductormusica/HTTP_Mongo.kt
@@ -1,5 +1,42 @@
 package cat.boscdelacoma.reproductormusica
 
-class HTTP_Mongo {
+import com.github.kittinunf.fuel.Fuel
+import java.io.File
 
+class HTTP_Mongo {
+    private val apiUrl = "URL_DE_TU_API_MONGODB"
+
+    /**
+     * Metodo que ens permet descarregar una cançó de l'api de mongo
+     * @param songId Id de la cancion
+     * @return {ByteArray} Retorna un array de bytes con la cancion
+     * */
+    fun downloadSongsFromAPI(songId: String): ByteArray? {
+        val (_, response, result) = Fuel.get("$apiUrl/songs/$songId")
+            .response()
+        return when (response.statusCode) {
+            200 -> result.component1()
+            else -> {
+                null
+            }
+        }
+    }
+
+    /**
+     * Metode que ens permet pujar una cançó a l'api de mongo
+     * @param filePath Ruta de la cançó
+     * @return {Boolean} Retorna true si s'ha pujat la cançó
+     * */
+    fun uploadSongToAPI(filePath: String): Boolean {
+        val songFile = File(filePath)
+        if (!songFile.exists()) {
+            return false
+        }
+
+        val (_, response, _) = Fuel.post("$apiUrl/songs")
+            .body(songFile)
+            .response()
+
+        return response.statusCode == 200
+    }
 }
