ebd7b49595b9d1d71888ee7d7d3f648a5bfda35f
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 19:12:32 +0100

    Renombrar atributs de la classe Agrupa

--- API_SQL/Model/Agrupa.cs
+++ API_SQL/Model/Agrupa.cs
@@ -4,8 +4,9 @@ namespace dymj.ReproductorMusica.API_SQL.Model {
 public class Agrupa
 {
     public DateTime DataIncorporacio { get; set; }
-    public int IDGrup { get; set; }
-    public int IDMusic { get; set; }
+    public string NomGrup { get; set; }
+    public DateTime DataFundacioGrup { get; set; }
+    public int NomMusic { get; set; }
     public Grup GrupObj { get; set; }
     public Music MusicObj { get; set; }
 }
