5941c19b557a5dd380709451247d6be5ff8a9fae
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 17:08:14 +0100

    Modificar la classe de Grup per deixar de tenir una PK

--- API_SQL/Model/Grup.cs
+++ API_SQL/Model/Grup.cs
@@ -2,7 +2,6 @@ using System.ComponentModel.DataAnnotations;
 
 namespace dymj.ReproductorMusica.API_SQL.Model {
     public partial class Grup {
-        [Key]
         [MaxLength(30)]
         public string Nom {get; set;}
         public string DataFundacio {get; set;}
