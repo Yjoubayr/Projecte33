6dd793833d41b849a4201f414c34ef9970d829c7
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 17:08:47 +0100

    Modificacions en el arxiu de GrupConfiguracio per señalitzar que tenim una PK composta

--- API_SQL/Data/Configurations/GrupConfiguration.cs
+++ API_SQL/Data/Configurations/GrupConfiguration.cs
@@ -6,6 +6,7 @@ namespace dymj.ReproductorMusica.API_SQL.Configuration {
     public class GrupConfiguracio : IEntityTypeConfiguration<Grup> {
         public void Configure(EntityTypeBuilder<Grup> builder)
         {
+            builder.HasKey(g => new{g.Nom, g.DataFundacio});
         }
     }
 }
