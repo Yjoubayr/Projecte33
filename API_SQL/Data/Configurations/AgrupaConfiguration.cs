cb2cad0a8a43bdd01e368505be1516a03d434037
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 19:14:45 +0100

    Declarar clau primària de la classe Agrupar

--- API_SQL/Data/Configurations/AgrupaConfiguration.cs
+++ API_SQL/Data/Configurations/AgrupaConfiguration.cs
@@ -6,9 +6,9 @@ namespace dymj.ReproductorMusica.API_SQL.Configuration {
     public class AgrupaConfiguration : IEntityTypeConfiguration<Agrupa> {
         public void Configure(EntityTypeBuilder<Agrupa> builder)
         {
-            builder.HasKey(a => new {a.IDGrup, a.IDMusic, a.DataIncorporacio});
-            builder.HasOne(a => a.GrupObj).WithMany(g => g.LAgrupes).HasForeignKey(a => a.IDGrup);
-            builder.HasOne(a => a.MusicObj).WithMany(m => m.LAgrupes).HasForeignKey(a => a.IDMusic);
+            builder.HasKey(a => new {a.NomGrup,a.DataFundacioGrup ,a.NomMusic, a.DataIncorporacio});
+            // builder.HasOne(a => a.GrupObj).WithMany(g => g.LAgrupes).HasForeignKey(a => a.IDGrup);
+            // builder.HasOne(a => a.MusicObj).WithMany(m => m.LAgrupes).HasForeignKey(a => a.IDMusic);
         }
     }
 }
