fbfb59aa6a456853fd38858a3eb366458f819fd7
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 18:34:22 +0100

    Possible solucio final a la relacio de la  relacio entre Music  grup i agrupa

--- API_SQL/Data/Configurations/AgrupaConfiguration.cs
+++ API_SQL/Data/Configurations/AgrupaConfiguration.cs
@@ -6,7 +6,7 @@ namespace dymj.ReproductorMusica.API_SQL.Configuration {
     public class AgrupaConfiguration : IEntityTypeConfiguration<Agrupa> {
         public void Configure(EntityTypeBuilder<Agrupa> builder)
         {
-            builder.HasKey(a => new {a.IDGrup, a.IDMusic });
+            builder.HasKey(a => new {a.IDGrup, a.IDMusic, a.DataIncorporacio});
             builder.HasOne(a => a.GrupObj).WithMany(g => g.LAgrupes).HasForeignKey(a => a.IDGrup);
             builder.HasOne(a => a.MusicObj).WithMany(m => m.LAgrupes).HasForeignKey(a => a.IDMusic);
         }
--- API_SQL/Model/Agrupa.cs
+++ API_SQL/Model/Agrupa.cs
@@ -3,6 +3,7 @@ using System.ComponentModel.DataAnnotations;
 namespace dymj.ReproductorMusica.API_SQL.Model {
 public class Agrupa
 {
+    public DateTime DataIncorporacio { get; set; }
     public int IDGrup { get; set; }
     public int IDMusic { get; set; }
     public Grup GrupObj { get; set; }
