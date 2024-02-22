7013b4b9269500f19d901a235a4e8a8d1bb9f360
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 19:15:45 +0100

    Afegir clau primària de Tocar i comentar  codi de FK

--- API_SQL/Data/Configurations/TocarConfiguration.cs
+++ API_SQL/Data/Configurations/TocarConfiguration.cs
@@ -6,11 +6,11 @@ namespace dymj.ReproductorMusica.API_SQL.Configuration {
     public class TocarConfiguration : IEntityTypeConfiguration<Tocar> {
         public void Configure(EntityTypeBuilder<Tocar> builder)
         {
-            builder.HasKey(t => new { t.IDCanco, t.NomMusic, t.NomGrup, t.NomInstrument });
-            builder.HasOne(t => t.CancoObj).WithMany(c => c.LTocar).HasForeignKey(t => t.IDCanco);
+            builder.HasKey(t => new { t.IDCanco, t.NomMusic, t.NomGrup, t.DataFundacioGrup, t.NomInstrument });
+            /* builder.HasOne(t => t.CancoObj).WithMany(c => c.LTocar).HasForeignKey(t => t.IDCanco);
             builder.HasOne(t => t.MusicObj).WithMany(m => m.LTocar).HasForeignKey(t => t.NomMusic);
             builder.HasOne(t => t.GrupObj).WithMany(g => g.LTocar).HasForeignKey(t => t.NomGrup);
-            builder.HasOne(t => t.InstrumentObj).WithMany(i => i.LTocar).HasForeignKey(t => t.NomInstrument);
+            builder.HasOne(t => t.InstrumentObj).WithMany(i => i.LTocar).HasForeignKey(t => t.NomInstrument); */
         }
     }
 }
