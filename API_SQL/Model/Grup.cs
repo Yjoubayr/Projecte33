a98ac75fdcc4883db94aa499d85a07d37f3192d0
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 19:11:26 +0100

    Modificar el tipus de l'atribut de DataFundacio a DateTime

--- API_SQL/Model/Grup.cs
+++ API_SQL/Model/Grup.cs
@@ -4,7 +4,7 @@ namespace dymj.ReproductorMusica.API_SQL.Model {
     public partial class Grup {
         [MaxLength(30)]
         public string Nom {get; set;}
-        public string DataFundacio {get; set;}
+        public DateTime DataFundacio {get; set;}
         public int? Any { get; set; }
         public ICollection<Tocar>? LTocar { get; set; } = new List<Tocar>();
         public ICollection<Agrupa>? LAgrupes {get; set;} = new List<Agrupa>();
