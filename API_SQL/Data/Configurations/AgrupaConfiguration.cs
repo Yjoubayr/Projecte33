0395d4bf14fb7b5116c0a49923266268ddf90216
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-22 17:21:09 +0100

    Modificacions en la taula de Agrupa  declarar les claus compostes i agafar  els atributs d'altres taules

--- API_SQL/Data/Configurations/AgrupaConfiguration.cs
+++ API_SQL/Data/Configurations/AgrupaConfiguration.cs
@@ -6,6 +6,9 @@ namespace dymj.ReproductorMusica.API_SQL.Configuration {
     public class AgrupaConfiguration : IEntityTypeConfiguration<Agrupa> {
         public void Configure(EntityTypeBuilder<Agrupa> builder)
         {
+            builder.HasKey(a => new { a.DataIncorporacio, a.IDGrup, a.IDMusic });
+            builder.HasOne(a => a.GrupObj).WithMany(g => g.LAgrupes).HasForeignKey(a => a.IDGrup);
+            builder.HasOne(a => a.MusicObj).WithMany(m => m.LAgrupes).HasForeignKey(a => a.IDMusic);
         }
     }
 }
