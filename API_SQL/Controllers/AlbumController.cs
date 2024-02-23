7cb8893842d76298a93feae77f25f3665b7884be
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-23 16:16:09 +0100

    Descomentar el codi de AlbumController

--- API_SQL/Controllers/AlbumController.cs
+++ API_SQL/Controllers/AlbumController.cs
@@ -1,4 +1,4 @@
-/*using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -191,4 +191,4 @@ namespace dymj.ReproductorMusica.API_SQL.Controller
             return NoContent();
         }
     }
-}*/
+}
