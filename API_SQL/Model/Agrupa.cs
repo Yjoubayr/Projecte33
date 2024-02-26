5a24b0bb58f071cb1ee0f8f1091e47d9f12f9e28
aimar-cortes <acortes660@boscdelacoma.cat>
2024-02-26 15:34:35 +0100

    Canviar el tipus de NomMusic a string per solucionar un error

--- API_SQL/Model/Agrupa.cs
+++ API_SQL/Model/Agrupa.cs
@@ -6,7 +6,7 @@ public class Agrupa
     public DateTime DataIncorporacio { get; set; }
     public string NomGrup { get; set; }
     public DateTime DataFundacioGrup { get; set; }
-    public int NomMusic { get; set; }
+    public string NomMusic { get; set; }
     public Grup GrupObj { get; set; }
     public Music MusicObj { get; set; }
 }
