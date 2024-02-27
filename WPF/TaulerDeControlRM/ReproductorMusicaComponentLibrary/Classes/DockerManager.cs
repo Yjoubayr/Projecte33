03426e1cfe07efe8304d853b02daf278277201ff
Casta16m <102661672+Casta16m@users.noreply.github.com>
2024-02-27 19:39:37 +0100

    Noves funcions de DockerManager

--- WPF/TaulerDeControlRM/ReproductorMusicaComponentLibrary/Classes/DockerManager.cs
+++ WPF/TaulerDeControlRM/ReproductorMusicaComponentLibrary/Classes/DockerManager.cs
@@ -9,17 +9,58 @@ namespace ReproductorMusicaComponentLibrary.Classes
 {
     internal class DockerManager
     {
-        public static void DescargarSqlServer()
+        public static void DescarregarMicrosoftSqlServer()
         {
-            //EjecutarComandoDocker("pull mcr.microsoft.com/mssql/server:2022-latest");
+            ExecutarComandaDocker("pull mcr.microsoft.com/mssql/server:2022-latest");
         }
 
-        public static void EjecutarSqlServerContenedor(string nombreContenedor, string saPassword)
+        public static void DescarregarMySqlServer()
+        {
+            ExecutarComandaDocker("");
+        }
+
+        public static void DescarregarPostgresSqlServer()
+        {
+
+        }
+
+        public static void ExecutarContenidorMicrosoftSqlServer(string nameContenidor, string saPassword)
         {
             // Ajusta la cadena de conexión según tus necesidades
-            string cadenaConexion = $"-e \"ACCEPT_EULA=Y\" -e \"SA_PASSWORD={saPassword}\"";
+            string cadenaConexio = $"-e \"ACCEPT_EULA=Y\" -e \"SA_PASSWORD={saPassword}\"";
 
-            //EjecutarComandoDocker($"run --name {nombreContenedor} {cadenaConexion} -d mcr.microsoft.com/mssql/server:2022-latest");
+            ExecutarComandaDocker($"run --name {nameContenidor} {cadenaConexio} -d mcr.microsoft.com/mssql/server:2022-latest");
+        }
+
+
+
+        private static void ExecutarComandaDocker(string comanda)
+        {
+            try
+            {
+                ProcessStartInfo startInfo = new ProcessStartInfo
+                {
+                    FileName = "docker",
+                    Arguments = comanda,
+                    RedirectStandardOutput = true,
+                    UseShellExecute = false,
+                    CreateNoWindow = true
+                };
+
+                using (Process proces = new Process { StartInfo = startInfo })
+                {
+                    proces.Start();
+
+                    string resultat = proces.StandardOutput.ReadToEnd();
+                    Console.WriteLine(resultat);
+
+                    proces.WaitForExit();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error al executar la comanda Docker: {ex.Message}");
+            }
         }
     }
 }
