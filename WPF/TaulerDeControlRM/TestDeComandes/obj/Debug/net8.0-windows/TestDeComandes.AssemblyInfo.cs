fb2d6863ab50c9c19140482be6fdf2879029d91f
Casta16m <102661672+Casta16m@users.noreply.github.com>
2024-02-29 12:17:45 +0100

    Proves de primera execucio amb app

--- WPF/TaulerDeControlRM/TestDeComandes/obj/Debug/net8.0-windows/TestDeComandes.AssemblyInfo.cs
+++ WPF/TaulerDeControlRM/TestDeComandes/obj/Debug/net8.0-windows/TestDeComandes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestDeComandes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+03426e1cfe07efe8304d853b02daf278277201ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+be967e798d0e7af60e28e3c71215fec3b811bcff")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestDeComandes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestDeComandes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
